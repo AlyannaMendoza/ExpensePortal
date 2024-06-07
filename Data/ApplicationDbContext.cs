3dbd1e020103f3d4efecfa352bf2d2fd2fbde7ac
AlyannaMendoza <102855587+AlyannaMendoza@users.noreply.github.com>
2024-06-07 16:32:20 +0800

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SPEND_SMART.Models;
+
+namespace SPEND_SMART.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+        }
+        public DbSet<SPEND_SMART.Models.Expense> Expense { get; set; } = default!;
+        public DbSet<SPEND_SMART.Models.Category> Category { get; set; } = default!;
+        public DbSet<SPEND_SMART.Models.Income> Income { get; set; } = default!;
+    }
+}
